4eb88e7f9e029dcd6353ff2eeb96b839c42ba761
HeatherKolinskyBSU <146090360+HeatherKolinskyBSU@users.noreply.github.com>
2023-09-28 15:26:38 +0100

    Ch1-Ex2
    Exercise 2 from Chapter 1 of the pre-lecture exercises. Complete working code with annotations.

---
+++ Ch1-Ex2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {                          // Denotes main function.
+    int firstNumber = 1;              // Assigns 1 to firstNumber using int.
+    bool iCanCode = true;             // Assigns true to iCanCode using bool.
+    const char hopefulGrade = 'a';    // Assigns 'a' to hopefulGrade using const char.
+    double myDecimal = 1.0;           // Assigns 1.0 to MyDecimal using double.
+    string minimalSentance = "y";     // Assigns "y" to minimalSentance using string.
+    int keyMash = 13213123;           // Assigns 132132123 to keyMash using int.
+    float mysteryDataType = 5.6f;     // Assigns 5.6f to mysteryDataType using float (the mystery has been unveiled).
+
+    return 0;                         // Return statement.
+}                                     // Ends main function.
