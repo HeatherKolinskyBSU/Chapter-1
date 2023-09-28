188a3e4068f526de408ac759333f3b412726a00e
HeatherKolinskyBSU <146090360+HeatherKolinskyBSU@users.noreply.github.com>
2023-09-28 14:24:18 +0100

    Ch1-Ex1
    Exercise 1 from Chapter 1 of the pre-lecture exercises. Complete working code with annotations.

---
+++ Ch1-Ex1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {                                 // Denotes main function.
+      cout << "Coding is Cool" << endl;      // Outputs desired text to screen.
+}                                            // Ends main function.
